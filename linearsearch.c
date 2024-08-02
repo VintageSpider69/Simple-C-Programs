ab6030100f7edb02581217ef5e2b0147fd939241
Rayid Mohammed Afzal <168258941+VintageSpider69@users.noreply.github.com>
2024-08-02 20:29:30 +0530

    linearsearch.c

---
+++ linearsearch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+int i, n, key;
+ printf ("Enter the value of N: \n");
+ scanf("%d", &n);
+ int array[n];
+ printf("Enter %d numbers: \n", n);
+ for (i = 0; i < n; i++)
+ {
+   scanf("%d", &array[i]);
+ }
+ printf("Enter element to search: \n");
+ scanf("%d", &key);
+ for(i = 0; i < n; i++)
+ {
+  if(array[i] == key)
+   {
+     break;
+   }
+ }
+ if(i < n)
+ {
+   printf("Element found at position %d\n", i + 1);
+ }
+ else
+ {
+   printf("Element not found\n");
+ }
+ return 0;
+}
